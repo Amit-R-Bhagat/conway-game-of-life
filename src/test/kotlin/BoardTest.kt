95cd3634557167dcb7883b0326d0eab52cca24b6
Amit Bhagat <amitb@sahaj.ai>
2023-02-24 15:29:27 +0530

    Add test to CellTest and BoardTest

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,3 +1,21 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class BoardTest
+class BoardTest{
+
+    @Test
+    fun `should create a board`(){
+        var cellList:ArrayList<Cell> = ArrayList()
+        for(row in 0 until 4){
+            for(col in 0 until  4){
+                cellList.add(Cell(row , col, true))
+            }
+        }
+
+        var board = Board(4, 4, cellList)
+
+        assertEquals(board.getCellList().size,16)
+
+    }
+
+}
--- src/test/kotlin/CellTest.kt
+++ src/test/kotlin/CellTest.kt
@@ -1,3 +1,21 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class CellTest
+class CellTest {
+
+    @Test
+    fun `should return true if cell is alive`(){
+        val cell = Cell(0,0,true)
+
+        assertEquals(true, cell.isAlive())
+    }
+
+    @Test
+    fun `should toggle living state`(){
+        val cell = Cell(0,0,true)
+
+        cell.toggleLiving()
+
+        assertEquals( false , cell.isAlive())
+    }
+}
