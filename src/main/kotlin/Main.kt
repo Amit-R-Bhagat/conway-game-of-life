4b563119dfbdfb7566828285894d86dfdb6ce646
Amit Bhagat <amitb@sahaj.ai>
2023-02-24 13:13:47 +0530

    Create update method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,44 @@
 fun main(args: Array<String>) {
     val board = getBoard()
+    updateBoard(board)
     printBoard(board)
+}
+
+fun updateBoard(board: Array<IntArray>) {
+    for((rowNumber, row) in board.withIndex()){
+        for((colNumber, cell) in row.withIndex()){
+            var numberOfNeighbours = getNumberOfNeighbours(rowNumber, colNumber, board)
+
+            if(board[rowNumber][colNumber]==1){
+                if(numberOfNeighbours< 2 || numberOfNeighbours > 3){
+                    board[rowNumber][colNumber] = 0
+                }
+            }else{
+                if(numberOfNeighbours==3){
+                    board[rowNumber][colNumber] = 1
+                }
+            }
+        }
+    }
+}
+
+fun getNumberOfNeighbours(rowNumber: Int, colNumber: Int, board: Array<IntArray>): Int {
+    var neighbourCount = 0
+    var rowOffsetList = listOf<Int>(0, 1, -1)
+    var colOffsetList = listOf<Int>(0, 1, -1)
+
+    for(rowOffset in rowOffsetList){
+        for(colOffset in colOffsetList){
+            if(rowOffset == 0 && colOffset == 0) continue
+            if(isExist(rowNumber+rowOffset, colNumber+colOffset, board) && board[rowNumber][colNumber] == 1) neighbourCount++
+        }
+    }
+    return  neighbourCount
+}
+
+fun isExist(rowNumber: Int, colNumber: Int, board: Array<IntArray>): Boolean {
+    if(rowNumber >= board.size || colNumber >= board[0].size) return false
+    return true
 }
 
 private fun getBoard(): Array<IntArray> {
